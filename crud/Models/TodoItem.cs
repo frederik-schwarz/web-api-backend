522d35bafbd10e840119105848b6a50cbdbd04ad
Frederik <frederik@jens-schwarz.com>
2023-04-17 08:54:00 +1200

    models

---
+++ crud/Models/TodoItem.cs
@@ -0,0 +1,10 @@
+namespace TodoApi.Models;
+
+public class TodoItem
+{
+    public long Id { get; set; }
+    public string? Name { get; set; }
+    public bool IsComplete { get; set; }
+}
+
+https://learn.microsoft.com/en-us/aspnet/core/tutorials/first-web-api?view=aspnetcore-7.0&tabs=visual-studio-code
